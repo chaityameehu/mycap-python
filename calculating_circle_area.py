7ddf65e6736f72380657036714d3cae3fca301fc
chaityameehu <51320468+chaityameehu@users.noreply.github.com>
2023-07-06 21:35:36 +0530

    Add files via upload

---
+++ calculating_circle_area.py
@@ -0,0 +1,3 @@
+Radius = float(input('Input the radius of the circle : '))
+Area = 3.14*Radius*Radius
+print('The are of the circle with the radius ' + str(Radius) + ' is:' + str(Area))
---
+++ positive number program.py
@@ -0,0 +1,5 @@
+list1 = [12, -7, 5, 64, -14]
+print('Postive number from the list is :')
+for num in list1:
+    if num > 0:
+        print(num)
